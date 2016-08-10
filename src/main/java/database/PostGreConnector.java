f387c13ed936ce87b1087d439a35f82efd775f8a
Ziolass <m.ziolkowski@stud.elka.pw.edu.pl>
2016-08-10 20:49:20 +0200

    Updated gitignore

--- src/main/java/database/PostGreConnector.java
+++ src/main/java/database/PostGreConnector.java
@@ -1,5 +1,7 @@
 package database;
 
+import java.sql.*;
+
 /**
  * Created by Michau on 02.08.2016.
  */
@@ -9,7 +11,33 @@ public class PostGreConnector implements DatabaseConnector{
     public static PostGreConnector getInstance() {
         return ourInstance;
     }
-
+    private Connection connection;
     private PostGreConnector() {
+        try {
+            Class.forName("org.postgresql.Driver");
+
+        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bachelor",
+                        "bachelor", "bachelor");
+
+        }catch (ClassNotFoundException cEx){
+            cEx.printStackTrace();
+        }catch (SQLException sqlEx){
+            sqlEx.printStackTrace();
+        }
+
+    }
+    public String insertRecord(String message, int id){
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet =null;
+        try {
+            preparedStatement = connection.prepareStatement("INSERT INTO tweets VALUES (?,?)");
+            preparedStatement.setString(1,message);
+            preparedStatement.setInt(2,id);
+            resultSet = preparedStatement.executeQuery();
+        }catch (SQLException sqlE){
+            sqlE.printStackTrace();
+        }
+        return resultSet.toString();
+
     }
 }
